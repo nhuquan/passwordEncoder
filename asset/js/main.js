55144b5c29013446ff65f0d3bbb45dd1ce930c91
nhu-quan.dang <nhu-quan.dang@eptica.com>
2018-06-22 16:38:35 +0200

    add offline password gen

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,8 +1,33 @@
-function encode() {
-  var input = document.getElementById('encodetext');
-  var output = document.getElementById('decodetext');
 
-  var password = "PWD" + input.value.replace(/\s+/g," ")
+var adj= {
+  "en": [
+    "pretty", "ugly","quick",
+    "beauty", "smart","bad", "good", "strange",
+    "funny", "slow", "big", "small",
+    "old", "new","humble", "powerful", "fast",
+    "clever", "cool", "cold", "warm"
+  ],
+  "vi": [
+    "nhanh", "cham", "xinh","xau","dep","tot", "la","moi", "cu",
+    "khiemton", "manhme", "huhong", "nong", "lanh", "matme"
+  ]
+}
+var noun = {
+  "en" : [
+    "banana","apple", "dog", "cat", "boat", "man", "girl", "boy", "woman", "tank",
+    "chicken", "ship", "flower", "bunny", "matrix", "godzilla", "bat",
+    "book", "gun", "house", "island", "cloud", "robo", "machine",
+    "paper", "pen", "phone", "string", "helmet", "ear", "dolphin", "plastic",
+    "rock", "hammer", "heart"
+  ],
+  "vi": [
+    "chuoi", "tao", "cho", "meo", "tau", "trai", "gai", "xetang","ga","hoa",
+    "tho", "doi", "sach","sung", "nha", "dao", "may", "nguoimay", "giay", "but",
+    "mu", "tai", "ca", "bua"
+  ]
+}
+function encodeText(input) {
+  return input.replace(/\s+/g," ")
                                 .replace(/a/gi,"@")
                                 .replace(/i/gi,"!")
                                 .replace(/t/gi,"+")
@@ -11,13 +36,18 @@ function encode() {
                                 .replace(/e/gi,"3")
                                 .replace(/z/gi,"2")
                                 .replace(/f/gi,"=")
-                                .replace(/ /gi,"_")
-                            + Math.floor(Math.random(99) * 100);
+                                .replace(/ /gi,"_");
+}
+function encode() {
+  var input = document.getElementById('encodetext');
+  var output = document.getElementById('decodetext');
+
+  var password = "PWD" + encodeText(input.value) + '00';
 
   output.value = password;
   input.value = '';
+  document.getElementById('notif').style.display="none";
 }
-
 
 function decode() {
   var input = document.getElementById('decodetext');
@@ -36,23 +66,33 @@ function decode() {
                       .replace(/_/gi," ")
   output.value = decoded;
   input.value = '';
+  document.getElementById('notif').style.display="none";
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
 function generate() {
-  var xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById('genpass').innerText = this.responseText;
-        var elm = document.getElementById('encodetext');
-        elm.value = this.responseText;
-        elm.select();
-        document.execCommand('cut');
-        document.getElementById('pwdmodal').classList.add('is-active');
-    }
+  var lang = document.querySelector('.language:checked').value
+  var word, a, b;
+  if (lang === 'en') {
+    a = capitalize(adj.en[getRandomInt(adj.en.length)]);
+    b = capitalize(noun.en[getRandomInt(noun.en.length)]);
+  } else {
+    b = capitalize(adj.vi[getRandomInt(adj.vi.length)]);
+    a = capitalize(noun.vi[getRandomInt(noun.vi.length)]);
   }
-
-  xhr.open('GET', 'http://www.dinopass.com/password/strong',false)
-  xhr.send();
+  word = encodeText(a + b) + getRandomInt(99);
+  document.getElementById('generatedText').innerText = word;
+  document.getElementById('encodetext').value = word;
+  document.getElementById('encodetext').select();
+  document.execCommand('cut');
+  document.getElementById('encodetext').blur();
+  document.getElementById('notif').style.display="block";
 }
 
 function closeModal() {
